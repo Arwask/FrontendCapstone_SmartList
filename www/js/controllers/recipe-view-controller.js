14ce192a1559741edf7068ae6eec623b132dfa39
Arwa <arwa.it15@gmail.com>
2017-10-09 17:35:33 -0500

    hiding api creds for deploying online

--- www/js/controllers/recipe-view-controller.js
+++ www/js/controllers/recipe-view-controller.js
@@ -1,49 +1,49 @@
 'use strict';
 
-smartApp.controller('RecipeViewCtrl', function($scope, $window,$q,$ionicLoading, RecipeFactory) {
+smartApp.controller('RecipeViewCtrl', function($scope, $window, $q, $ionicLoading, RecipeFactory) {
+  $scope.NavTitle = 'Browse Recipes';
 
-    $scope.NavTitle = "Browse Recipes";
-    
-    $scope.show = function() {
+  $scope.show = function() {
     $ionicLoading.show({
-      template:'<ion-spinner></ion-spinner>'
+      template: '<ion-spinner></ion-spinner>'
     });
-    };
+  };
 
-     $scope.hide = function(){
-        $ionicLoading.hide();
-    };
+  $scope.hide = function() {
+    $ionicLoading.hide();
+  };
 
-    $scope.show($ionicLoading)
-    RecipeFactory.get10Recipes()
-    .then( (recievedRecipes) => {
-        $scope.recipes = recievedRecipes.data.recipes;
-        $scope.hide($ionicLoading);
+  $scope.show($ionicLoading);
+  RecipeFactory.get10Recipes()
+    .then(recievedRecipes => {
+      console.log('reciecedRecipes', recievedRecipes);
+      $scope.recipes = recievedRecipes.data.recipes;
+      $scope.hide($ionicLoading);
     })
-    .catch( (err) => {
-        console.log("Error",err );
-    })
-    // .finally( ($ionicLoading) => {
-    // })
+    .catch(err => {
+      console.log('Error', err);
+    });
+  // .finally( ($ionicLoading) => {
+  // })
 
-    let recipeIdArray = [];
+  let recipeIdArray = [];
 
-    $scope.searchedRecipe = (searchText) => {
-        $scope.show($ionicLoading); 
-        RecipeFactory.searchedRecipes(searchText)
-        .then( (searchedData) => {
-            searchedData.data.forEach( (recipe) => {
-                recipeIdArray.push(recipe.id);
-            });
-            RecipeFactory.getRecipeById(recipeIdArray)  
-            .then( (Recipedata) => {
-                $scope.recipes = Recipedata;
-                $scope.hide($ionicLoading);
-            });
-        })
-        .catch( (err) => {
-            console.log("err",err );
+  $scope.searchedRecipe = searchText => {
+    // $scope.show($ionicLoading);
+    // console.log(searchText);
+    RecipeFactory.searchedRecipes(searchText)
+      .then(searchedData => {
+        console.log('SearchedData', searchedData);
+        searchedData.data.forEach(recipe => {
+          recipeIdArray.push(recipe.id);
         });
-    };
-
+        RecipeFactory.getRecipeById(recipeIdArray).then(Recipedata => {
+          $scope.recipes = Recipedata;
+          $scope.hide($ionicLoading);
+        });
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  };
 });
--- www/js/factories/recipe-factory.js
+++ www/js/factories/recipe-factory.js
@@ -1,95 +1,104 @@
 'use strict';
 
-smartApp.factory('RecipeFactory', function($q, $http, RecipeCreds) {
-
-        // let myRequest = new XMLHttpRequest();
-    function get10Recipes() {
-    return $q( (resolve, reject) => {
-        // ----working API call-----
-        $http({                          
-            method:"GET",
-            url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=10&tags=dessert`,
-            headers: {
-                "x-mashape-key": RecipeCreds.apiKey,
-                "cache-control": "no-cache"
-              }
-        }).
-        then( (AllData) => {
-            // console.log("data", AllData );
-            resolve(AllData);
-        })
-        .catch( (err) => {
-            console.log("Error:",err );
-        }); 
+smartApp.factory('RecipeFactory', function($q, $http, fbDataFactory) {
+  // The API creds are stored in firebase and the key is retrieved by making a request to firebase
+  function RecipeCreds() {
+    return $q((resolve, reject) => {
+      fbDataFactory.getRecipeCreds().then(creds => {
+        let key = Object.keys(creds.data);
+        let apiKey = creds.data[key].apiKey;
+        resolve(apiKey);
+      });
     });
-}
-    let searchedRecipes = (searchstring) => {
-        return $q( (resolve, reject) => {
-            // working
-            $http({
-                method:"GET",
-                url: ` https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query=${searchstring}`,
-                headers: {
-                    "x-mashape-key": RecipeCreds.apiKey,
-                    "cache-control": "no-cache"
-                  }
-            })
-            .then( (searchedData) => {
-                // console.log("searchedData",searchedData );
-                resolve(searchedData);
+  }
 
-            });
+  //On load of the page, it randomly loads 10 Recipes
+  function get10Recipes() {
+    return $q((resolve, reject) => {
+      RecipeCreds().then(apiKey => {
+        $http({
+          method: 'GET',
+          url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=10&tags=dessert`,
+          headers: {
+            'x-mashape-key': apiKey,
+            'cache-control': 'no-cache'
+          }
+        })
+          .then(AllData => {
+            resolve(AllData);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+    });
+  }
+
+  //the key word that user enters on the search bar, the call is made for 10 recipes with that keyword
+  let searchedRecipes = searchstring => {
+    return $q((resolve, reject) => {
+      RecipeCreds().then(apiKey => {
+        // working
+        $http({
+          method: 'GET',
+          url: ` https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query=${searchstring}`,
+          headers: {
+            'x-mashape-key': apiKey,
+            'cache-control': 'no-cache'
+          }
+        }).then(searchedData => {
+          resolve(searchedData);
         });
-    };
+      });
+    });
+  };
 
-    let getRecipeById = (idArray) => {
-        let recipePromises = [];
-        return $q( (resolve, reject) => {
-            // working
-            for(let i=0;i<idArray.length;i++)
-            {
-                // console.log("idArray", idArray[i]);
-                $http({
-                method:"GET",
-                url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${idArray[i]}/information?`,
-                headers: {
-                    "x-mashape-key": RecipeCreds.apiKey,
-                    "cache-control": "no-cache"
-                  }
-                })
-                .then( (actualData) => {
-                // console.log("searchedData",actualData );
-                // resolve(actualData.data);
-                recipePromises.push(actualData.data);
-                resolve(recipePromises);
-                })
-                .catch( (err) => {
-                    console.log("err",err );
-                });     
+  // loads all the recipe's information using its IDs
+  let getRecipeById = idArray => {
+    let recipePromises = [];
+    return $q((resolve, reject) => {
+      RecipeCreds().then(apiKey => {
+        for (let i = 0; i < idArray.length; i++) {
+          $http({
+            method: 'GET',
+            url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${idArray[i]}/information?`,
+            headers: {
+              'x-mashape-key': apiKey,
+              'cache-control': 'no-cache'
             }
-        });
-    };
-
-
-    let getSingleRecipeById = (recipeId) => {
-        return $q( (resolve, reject) => {
-                $http({
-                method:"GET",
-                url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${recipeId}/information?`,
-                headers: {
-                    "x-mashape-key": RecipeCreds.apiKey
-                    }
-                })
-            .then( (actualData) => {
-                // console.log("searchedData",actualData );
-                // resolve(actualData.data);
-                resolve(actualData);
+          })
+            .then(actualData => {
+              recipePromises.push(actualData.data);
+              resolve(recipePromises);
             })
-            .catch( (err) => {
-                console.log("err",err );
+            .catch(err => {
+              reject(err);
             });
-        });
-    };
+        }
+      });
+    });
+  };
 
-return { get10Recipes, searchedRecipes, getRecipeById, getSingleRecipeById };
+  // getting a specific Recipe by its ID. used in single recipe view.
+  let getSingleRecipeById = recipeId => {
+    return $q((resolve, reject) => {
+      RecipeCreds().then(apiKey => {
+        $http({
+          method: 'GET',
+          url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${recipeId}/information?`,
+          headers: {
+            'x-mashape-key': apiKey
+          }
+        })
+          .then(actualData => {
+            resolve(actualData);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+    });
+  };
+
+  return { get10Recipes, searchedRecipes, getRecipeById, getSingleRecipeById };
 });

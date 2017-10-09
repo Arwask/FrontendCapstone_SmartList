c708c3beead2fa539443d4512d5552804e482993
Arwa <arwa.it15@gmail.com>
2017-10-09 18:03:31 -0500

    comment recipe-book-ctrl file

--- www/js/controllers/recipe-book-controller.js
+++ www/js/controllers/recipe-book-controller.js
@@ -1,39 +1,45 @@
 'use strict';
 
-smartApp.controller('RecipeBookCtrl', function($scope, $state, $window, $ionicLoading, fbDataFactory, UserFactory, RecipeFactory) {
-
-    $scope.NavTitle = "Recipe Book";
-    $scope.show = function() {
+smartApp.controller('RecipeBookCtrl', function(
+  $scope,
+  $state,
+  $window,
+  $ionicLoading,
+  fbDataFactory,
+  UserFactory,
+  RecipeFactory
+) {
+  $scope.NavTitle = 'Recipe Book';
+  $scope.show = function() {
     $ionicLoading.show({
-      template:'<ion-spinner></ion-spinner>'
+      template: '<ion-spinner></ion-spinner>'
     });
-    };
+  };
 
-     $scope.hide = function(){
-        $ionicLoading.hide();
-    };
+  $scope.hide = function() {
+    $ionicLoading.hide();
+  };
 
-    $scope.show();
-    UserFactory.isAuthenticated()
-    .then( (user) => {
-        let currentUser = UserFactory.getUser();   
-        fbDataFactory.getUserRecipes(currentUser)
-        .then( (userData) => {
-            let userRecipes = userData.data;
-            let idArray = [];
-            for(let key in userRecipes) {
-                idArray.push(userRecipes[key].recipe_id);
-            }
+  $scope.show();
+  // get user's recipe using current active user
+  UserFactory.isAuthenticated().then(user => {
+    let currentUser = UserFactory.getUser();
+    fbDataFactory.getUserRecipes(currentUser).then(userData => {
+      let userRecipes = userData.data;
+      let idArray = [];
+      for (let key in userRecipes) {
+        idArray.push(userRecipes[key].recipe_id);
+      }
 
-            RecipeFactory.getRecipeById(idArray)
-            .then( (recievedData) => {
-                $scope.hide();
-                $scope.recipes = recievedData;
-                $state.reload();
-            })
-            .catch( (err) => {
-                console.log("err",err );
-            });
+      RecipeFactory.getRecipeById(idArray)
+        .then(recievedData => {
+          $scope.hide();
+          $scope.recipes = recievedData;
+          $state.reload();
+        })
+        .catch(err => {
+          reject(err);
         });
-         });   
+    });
+  });
 });
